a5b389f46988d55d7c34e2aa47dc635940581c27
Medyassinef <ynabm2017@gmail.com>
2023-08-10 17:54:15 -0700

    shell

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**
- * * main -5 5-printf.c
- * * with proper grammar, but the outcome is a piece of art,
- * * Return: 0
+/*
+ *  main  
+ *  with proper grammar, but the outcome is a piece of art,
+ *  
  * */
 int main(void)
 {
