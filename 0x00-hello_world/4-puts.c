2433be965eb92d3a37c0b1377bd66980e172a9b5
Medyassinef <ynabm2017@gmail.com>
2023-08-10 17:51:18 -0700

    shell

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- * * main - 4-puts.c
- * * Programming is like building a multilingual puzzle
- * * Return: 0;
- * */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
